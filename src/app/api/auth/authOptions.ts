548fcb6ed451f767908c5de92b0d9f2a6e97cc35
oluyoung <kolugbake@hotmail.com>
2024-05-20 02:51:28 +0100

    use modular imports for mui icons

--- src/app/api/auth/authOptions.ts
+++ src/app/api/auth/authOptions.ts
@@ -2,6 +2,7 @@ import type { NextAuthOptions } from 'next-auth';
 import { Adapter } from 'next-auth/adapters';
 import GithubProvider, { GithubProfile } from 'next-auth/providers/github';
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
+import TwitterProvider, { TwitterProfile } from 'next-auth/providers/twitter';
 // import FacebookProvider from 'next-auth/providers/facebook';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prismaClient from '@/app/db/prisma-client';
@@ -28,6 +29,18 @@ export const authOptions = {
           id: profile.sub.toString(),
           email: profile.email,
           name: profile.name ?? profile.given_name,
+        };
+      },
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      version: '2.0',
+      profile(profile: TwitterProfile) {
+        return {
+          id: profile.data.id,
+          email: profile.data.username,
+          name: profile.data.name ?? profile.data.username,
         };
       },
     }),
